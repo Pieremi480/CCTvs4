b39a818f9d19883d792f8118bafa06636c434295
Glitch (itcs3u) <none>
2020-01-17 12:20:20 +0000

    ⏱🌘 Updated with Glitch

---
+++ public/client.js
@@ -0,0 +1,7 @@
+// function that will run when the self project link in the navigation bar
+function Function1() {
+  if (confirm("Are you sure you want to go to an escape room that you may not have a way out of")) {
+   location.replace("https://trapped-escape-game.herokuapp.com/")
+  } else {
+  }
+}
